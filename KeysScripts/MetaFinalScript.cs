73eba4dcc5d546a54e5a686d1b1c9d06a5964079
FabriBertani <fabri_bertani@hotmail.com>
2016-08-18 19:30:36 -0300

    Add files via upload

---
+++ KeysScripts/MetaFinalScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MetaFinalScript : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			SceneManager.LoadScene ("Menu");
+		}
+	}
+}
