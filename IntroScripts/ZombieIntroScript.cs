fe80f5821f3e9ef1ad1f6722e2e719c6ce68cf87
FabriBertani <fabri_bertani@hotmail.com>
2016-08-18 19:29:47 -0300

    Add files via upload

---
+++ IntroScripts/ZombieIntroScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieIntroScript : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetTrigger ("Intro");
+	
+	}
+}
